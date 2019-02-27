1b6686042bcde655cac2c7b19444d7345639b901
np-float-sand <sandhya.persad@gmail.com>
2019-02-27 17:58:50 -0500

    some methods3

--- src/selector.js
+++ src/selector.js
@@ -12,31 +12,38 @@ class $ {
         } else {
             this.elements = Array.from(document.getElementsByTagName(selector))
         }
+        
   }
   //hide()
   hide (){
       this.elements.forEach( (elem) => {elem.style.display = 'none'} );
+      return this;
   }
   
   show(){
     this.elements.forEach( (elem) => {elem.style.display = 'inherit'} );
+    return this;
   }
 
   addClassName(className){
     this.elements.forEach( (elem) => {elem.className = className} );
+    return this;
  }
 
  removeClassName(className){
      this.elements.forEach( (elem) => {elem.className = 'none'});
+     return this;
  }
 
  text(mytext){
     this.elements.forEach( (elem) => {elem.innerText = mytext} );
+    return this;
  }
 
  addChild(domType) {
      
      this.elements.forEach( (elem) => {elem.appendChild(document.createElement(domType))});
+     return this;
  }
 
  blink(interval){
@@ -51,8 +58,11 @@ class $ {
     }
     ,interval
     )
-    
+    //return this.elements;
+    return this;
 }
+
+
 
 }
 
