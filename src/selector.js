2cfe22bcaf48b176fe6ba59a6ed82360f155da01
Hariet <harietlewis@gmail.com>
2019-02-27 16:50:18 -0500

    Constructor

--- src/selector.js
+++ src/selector.js
@@ -1,1 +1,19 @@
-// YOUR CODE HERE
+class $ {
+    constructor(selector) {
+        if (selector.startsWith('.')) {
+            const className = selector.split('').slice(1).join('')
+            this.elements = Array.from(document.getElementsByClassName(className))
+        } else if (selector.startsWith('#')) {
+            const idName = selector.split('').slice(1).join('')
+            //console.log(idName)
+            this.elements = [document.getElementById(idName)]
+            //console.log(Array.isArray(this.elements), this.elements.length)
+            //console.log(this.elements)
+        } else {
+            this.elements = Array.from(document.getElementsByTagName(selector))
+        }
+  }
+  //hide()
+  //show()
+}
+
